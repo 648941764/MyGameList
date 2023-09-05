45af819db36d4da9922fdf04be7e906bc1535fd3
648941764 <648941764@qq.com>
2023-09-05 12:19:12 +0800

    1
    增加基本项目

---
+++ Assets/Scripts/InterFaceController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterFaceController : MonoBehaviour
+{
+    public Button StartBtn, SoundBtn, ExitBtn;
+}
